eb0ddc5b5e4eeb31faaa2a69fc31104a9d34a8f4
EdgarAgesa <edgarasige@gmail.com>
2024-10-14 01:09:40 +0300

    Deploy to render

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
 from flask_cors import CORS
+import os
 from models import db
 from resources import DayStockResource, NightStockResource, DayRecordResource
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
 api = Api(app)
 migrate = Migrate(app, db)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "postgresql://hotel_yyax_user:TTTVfmRliv4XtcuV52CU9Agh08OKz5B7@dpg-cs63kf08fa8c73asts90-a.oregon-postgres.render.com/hotel_yyax")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
